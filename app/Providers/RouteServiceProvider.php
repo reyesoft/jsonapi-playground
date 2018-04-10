94514a801022291d7ae91e5234623257b1b63974
Pablo Reyes <reyesoft@gmail.com>
2018-04-10 18:38:24 +0000

    Merged in fix/namespace (pull request #56)
    Fix/namespace
    
    * new runtests with -f option
    
    * runtests with -f option done
    
    * bash to sh
    
    * composer fixed
    
    * ide helper fixed
    
    * small fix
    
    * files like eje
    
    * packages updated
    
    * laravel updated to 5.6
    
    Approved-by: Pablo Reyes <reyesoft@gmail.com>
    Approved-by: Jonatan Eseiza <jonatan.eseiza@gmail.com>

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -15,26 +15,13 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * This declare(strict_types=1); namespace is applied to your controller routes.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
     protected $namespace = 'App\Http\Controllers';
 
-    /**
-     * Define your route model bindings, pattern filters, etc.
-     */
     public function boot(): void
     {
         parent::boot();
     }
 
-    /**
-     * Define the routes for the application.
-     */
     public function map(): void
     {
         $this->mapApiRoutes();
@@ -42,11 +29,6 @@ class RouteServiceProvider extends ServiceProvider
         $this->mapWebRoutes();
     }
 
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     */
     protected function mapWebRoutes(): void
     {
         Route::middleware('web')
@@ -54,11 +36,6 @@ class RouteServiceProvider extends ServiceProvider
             ->group(base_path('routes/web.php'));
     }
 
-    /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     */
     protected function mapApiRoutes(): void
     {
         Route::prefix('v2')
