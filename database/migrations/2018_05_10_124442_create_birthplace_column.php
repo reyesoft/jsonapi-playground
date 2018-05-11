dfa4886a872b4fd5f9f61bca3d281d1a107f52ac
juanicas23 <juanicastellano14@gmail.com>
2018-05-11 10:27:14 -0300

    fixed

--- database/migrations/2018_05_10_124442_create_birthplace_column.php
+++ database/migrations/2018_05_10_124442_create_birthplace_column.php
@@ -1,16 +1,16 @@
 <?php
 /**
-* Copyright (C) 1997-2018 Reyesoft <info@reyesoft.com>.
+ * Copyright (C) 1997-2018 Reyesoft <info@reyesoft.com>.
  *
  * This file is part of JsonApiPlayground. JsonApiPlayground can not be copied and/or
  * distributed without the express permission of Reyesoft
-*/
+ */
 
 declare(strict_types=1);
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateBirthplaceColumn extends Migration
 {
@@ -29,7 +29,7 @@ class CreateBirthplaceColumn extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down()
+    public function down(): void
     {
         Schema::table(
             'authors', function (Blueprint $table): void {
