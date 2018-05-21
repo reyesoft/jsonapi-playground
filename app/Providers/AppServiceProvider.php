15b9d1d16dfbeb7ced44ef24e6d425c5a2c3d99c
Pablo Reyes <reyesoft@gmail.com>
2018-05-21 11:04:23 +0000

    Merged in fix/boxfile (pull request #80)
    boxfile-fixed
    
    Approved-by: Pablo Reyes <reyesoft@gmail.com>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,5 +36,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 }
