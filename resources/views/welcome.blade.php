367bf13e92f93e6ba2a302a4683d8b855a1df726
Pablo Reyes <reyesoft@gmail.com>
2019-09-02 23:06:20 +0000

    Merged in fix/welcome-page-for-github (pull request #110)
    Fix/welcome page for github
    
    * first commit to github
    
    * text updated
    
    * auto commit
    
    * auto commit
    
    Approved-by: Pablo Reyes <reyesoft@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,7 @@
                     </li>
                     <li><span>GET</span> <a href="/v2/stores?filter[created_by]=1,3">/v2/stores?filter[created_by]=1,3</a></li>
                 </ul>
-                <p>More resources?
+                <p>More json:api resources?
                     <a href="/v2/books">books</a>
                     <a href="/v2/series">series</a>
                     <a href="/v2/chapters">chapters</a>
@@ -77,36 +77,36 @@
                     <a href="/v2/stores">stores</a>
                 </p>
 
-                <h2>More info</h2>
-                <ul>
-                    <li><a href="http://ngx-jsonapi.reyesoft.com/" target="_blank">Online JSON API client example ↗</a></li>
-                    <li><a href="http://jsonapi.org" target="_blank">JSON API Specification ↗</a></li>
-                    <li><a href="https://github.com/reyesoft/jsonapi-playground" target="_blank">Source code of this project ↗</a></li>
-                </ul>
-
                 <h3>Business rules</h3>
                 <ul>
                     <li>
                         chapters resource can't be created, updated or deleted.
-                        <a href="https://github.com/reyesoft/jsonapi-playground/blob/master/app/ChapterPolicy.php#L15" target="_blank">[↗]</a>
+                        <a href="https://github.com/reyesoft/jsonapi-playground/blob/master/app/ChapterPolicy.php#L15" target="_blank">↗</a>
                     </li>
                     <li>
                         stores.address can be only set on create, can't be updated.
-                        <a href="https://github.com/reyesoft/jsonapi-playground/blob/master/app/StoreSchema.php#L25" target="_blank">[↗]</a>
+                        <a href="https://github.com/reyesoft/jsonapi-playground/blob/master/app/StoreSchema.php#L25" target="_blank">↗</a>
                     </li>
                     <li>
                         stores.created_by can be only read.
-                        <a href="https://github.com/reyesoft/jsonapi-playground/blob/master/app/StoreSchema.php#L29" target="_blank">[↗]</a>
+                        <a href="https://github.com/reyesoft/jsonapi-playground/blob/master/app/StoreSchema.php#L29" target="_blank">↗</a>
                     </li>
                     <li>
                         chapter 1 cant be removed.
-                        <a href="https://github.com/reyesoft/jsonapi-playground/blob/master/app/AuthorPolicy.php#L15" target="_blank">[↗]</a>
+                        <a href="https://github.com/reyesoft/jsonapi-playground/blob/master/app/AuthorPolicy.php#L15" target="_blank">↗</a>
                     </li>
+                </ul>
+
+                <h2>More info</h2>
+                <ul>
+                    <li><a href="http://ngx-jsonapi.reyesoft.com/" target="_blank">Online JSON API client example</a></li>
+                    <li><a href="http://jsonapi.org" target="_blank">JSON API Specification</a></li>
+                    <li><a href="https://github.com/reyesoft/jsonapi-playground" target="_blank">Source code of this project</a></li>
                 </ul>
 
                 <p><small>NOTE: Information is reset every day.</small></p>
 
-                <p><small>Made with 💖 by <a href="https://www.reyesoft.com/" target="_blank">Reyesoft</a></small></p>
+                <p><small>Made and served with 💖 by <a href="https://www.reyesoft.com/" target="_blank">Reyesoft</a></small></p>
             </div>
         </div>
     </body>
